mkht/DHCPClient-PS

commit 90e915d79094d322e715f3682568b3f404126a70

Implement Encoding Long Options (RFC-3396)

Class/DhcpPacket.psm1
@@ -124,13 +124,20 @@ class DhcpPacket {
 
     [void]AddDhcpOptions([DhcpOptionObject[]]$Options) {
         foreach ($op in $Options) {
-            $this._DhcpOptionsList[$op.OptionCode] = $op
+            if ($null -eq $this._DhcpOptionsList[$op.OptionCode]) {
+                $this._DhcpOptionsList[$op.OptionCode] = $op
+            }
+            else {
+                $this._DhcpOptionsList[$op.OptionCode]._Value += $op._Value
+            }
         }
     }
 
     [void]AddDhcpOption([byte]$OptionCode, [byte[]]$Value) {
         $op = [DhcpOptionObject]::new([byte]$OptionCode, [byte[]]$Value)
-        $this._DhcpOptionsList[$op.OptionCode] = $op
+        $this.AddDhcpOptions($op)
+    }
+
     }
 
     static [DhcpPacket]Parse([byte[]]$Packet) {

Class/DhcpOptionObject.psm1
@@ -1,12 +1,15 @@
-﻿using module '.\Enums.psm1'
+using module '.\Enums.psm1'
 
 # DHCP Option object
 Class DhcpOptionObject {
     [byte]$OptionCode
     [string]$Name
 
-    [ValidateCount(0, 254)]
+    [ValidateCount(0, 1024)]
     Hidden [byte[]]$_Value
+
+    [ValidateRange(1, 255)]
+    [byte]$SplitSize = 255
 
     DhcpOptionObject([byte]$OptionCode, [byte[]]$Value) {
         $this.OptionCode = $OptionCode
@@ -38,12 +41,26 @@ Class DhcpOptionObject {
 
     [byte[]]GetBytes() {
         $ByteArray = New-Object 'System.Collections.Generic.List[byte]'
-        $ByteArray.Add($this.OptionCode)
-        $ByteArray.Add($this._Value.Count)
-        if ($null -ne $this._Value) {
-            $ByteArray.AddRange($this._Value)
+        if ($null -eq $this._Value) {
+            $ByteArray.Add($this.OptionCode)
+            $ByteArray.Add(0)
+            return $ByteArray.ToArray()
         }
-        return $ByteArray.ToArray()
+        else {
+            $Reader = [System.IO.BinaryReader]::new((New-Object IO.MemoryStream(@(, $this._Value))))
+            try {
+                (1..([math]::Ceiling($this._Value.Count / $this.SplitSize))) | ForEach-Object {
+                    $ByteArray.Add($this.OptionCode)
+                    $Length = [Math]::Min(($Reader.BaseStream.Length - $Reader.BaseStream.Position), $this.SplitSize)
+                    $ByteArray.Add($Length)
+                    $ByteArray.AddRange($Reader.GetBytes($Length))
+                }
+            }
+            finally {
+                $Reader.Close()
+            }
+            return $ByteArray.ToArray()
+        }
     }
 
     static [DhcpOptionObject]Parse([byte[]]$Bytes) {

commit f0e538bbf67c33ef8e3e2ae7918ca3769970581d

Fixed: packets are not correctly converted into byte arrays. This bug was introduced in 90e915d

Class/DhcpPacket.psm1
@@ -122,9 +122,9 @@ class DhcpPacket {
         }
     }
 
-    [void]AddDhcpOptions([DhcpOptionObject[]]$Options) {
+    [void]AddDhcpOptions([DhcpOptionObject[]]$Options, [bool]$ConcatWhenExist) {
         foreach ($op in $Options) {
-            if ($null -eq $this._DhcpOptionsList[$op.OptionCode]) {
+            if ((-not $ConcatWhenExist) -or ($null -eq $this._DhcpOptionsList[$op.OptionCode])) {
                 $this._DhcpOptionsList[$op.OptionCode] = $op
             }
             else {
@@ -133,9 +133,17 @@ class DhcpPacket {
         }
     }
 
-    [void]AddDhcpOption([byte]$OptionCode, [byte[]]$Value) {
+    [void]AddDhcpOptions([DhcpOptionObject[]]$Options) {
+        $this.AddDhcpOptions($Options, $false)
+    }
+
+    [void]AddDhcpOption([byte]$OptionCode, [byte[]]$Value, [bool]$ConcatWhenExist) {
         $op = [DhcpOptionObject]::new([byte]$OptionCode, [byte[]]$Value)
-        $this.AddDhcpOptions($op)
+        $this.AddDhcpOptions($op, $ConcatWhenExist)
+    }
+
+    [void]AddDhcpOption([byte]$OptionCode, [byte[]]$Value) {
+        $this.AddDhcpOption($OptionCode, $Value, $false)
     }
 
     [bool]RemoveDhcpOption([byte]$OptionCode) {
@@ -183,7 +191,7 @@ class DhcpPacket {
                     $OpLength = $Reader.ReadByte()
                     $OpValue = $Reader.ReadBytes($OpLength)
                     $OpsObj = [DhcpOptionObject]::new($OpNumber, $OpValue)
-                    $DhcpResponse.AddDhcpOptions($OpsObj)
+                    $DhcpResponse.AddDhcpOptions($OpsObj, $true)
                 }
             }
         }